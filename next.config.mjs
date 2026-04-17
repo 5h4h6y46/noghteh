2f801941773ebd4b1f258a9613e7b46f0344b297
Shahryar Ghasemzadeh <5h4h6y46@gmail.com>
2026-04-17 07:26:43 +0330

    Fix string quotes in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",       // ← tells Next.js to produce a static ./out folder
+  output: 'export',       // ← tells Next.js to produce a static ./out folder
   typescript: {
     ignoreBuildErrors: true,
   },
